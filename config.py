20356935fa1193c1386dc6f64153e54ca641f307
Nurbolat Bazarbek <47283347+Alaboy19@users.noreply.github.com>
2024-06-21 15:06:23 +0200

    Update config.py

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ REGISTERED_MODEL_NAME = "mlops-project-model"
 OWNER_NAME = "Alaboy19"
 REPOS_NAME = "model-retrain-gitops-fastapi"
 EVENT_TYPE = "retrain_pipeline"
-
+MLFLOW_TRACKING_URI = os.environ["MLFLOW_TRACKING_URI"]
 # put the correct service URI from the endpoint provided from step hosting MLflow on GCP, where whatever MLflow regsitry URI you have 
-os.environ["MLFLOW_TRACKING_URI"] = "https://my-cloud-run-service-1-{something_here}.a.run.app"
+#os.environ["MLFLOW_TRACKING_URI"] = "https://my-cloud-run-service-1-{something_here}.a.run.app"
 
 
